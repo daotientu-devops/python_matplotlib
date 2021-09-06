3db4f77a84c633b19f97eb727236bccbb7345065
tudao <tudao@5nhatnhat.com>
2021-09-06 15:39:24 +0700

    - [Matplotlib]: Histogram is using the hist() function to read array and produce

--- index.py
+++ index.py
@@ -2,11 +2,7 @@
 # The Pyplot package can be referred to as plt
 import matplotlib.pyplot as plt
 import numpy as np
-x = np.array(["A", "B", "C", "D"])
-y = np.array([3, 8, 1, 10])
-plt.bar(x, y, color="red", width=0.1)
-plt.show()
-x = np.array(["A", "B", "C", "D"])
-y = np.array([3, 8, 1, 10])
-plt.barh(x, y, color="#4CAF50", height=0.1)
+# this will generate a random result
+x = np.random.normal(170, 10, 250)
+plt.hist(x)
 plt.show()
