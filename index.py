1aba57a8473ec4cb30d010cf7b302a1176c7f4d1
tudao <tudao@5nhatnhat.com>
2021-09-06 15:13:45 +0700

    - [Matplotlib]: Scatter (compare plot, color, color each dot, colormap, size, alpha, combine color size and alpha)

--- index.py
+++ index.py
@@ -2,45 +2,29 @@
 # The Pyplot package can be referred to as plt
 import matplotlib.pyplot as plt
 import numpy as np
-# subplot() function to draw multiple plots in one figure
-# plot 1:
-x = np.array([0, 1, 2, 3])
-y = np.array([3, 8, 1, 10])
-# the figure has 1 row, 2 columns and this plot is the first plot
-plt.subplot(2, 3, 1)
-plt.plot(x, y)
-plt.title("SALES")
-# plot 2:
-x = np.array([0, 1, 2, 3])
-y = np.array([10, 20, 30, 40])
-# the figure has 1 row, 2 columns and this plot is the second plot
-plt.subplot(2, 3, 2)
-plt.plot(x, y)
-plt.title("INCOME")
-# plot 3:
-x = np.array([0, 1, 2, 3])
-y = np.array([3, 8, 1, 10])
-# the figure has 1 row, 2 columns and this plot is the 3 plot
-plt.subplot(2, 3, 3)
-plt.plot(x, y)
-# plot 4:
-x = np.array([0, 1, 2, 3])
-y = np.array([10, 20, 30, 40])
-# the figure has 1 row, 2 columns and this plot is the 4 plot
-plt.subplot(2, 3, 4)
-plt.plot(x, y)
-# plot 5:
-x = np.array([0, 1, 2, 3])
-y = np.array([3, 8, 1, 10])
-# the figure has 1 row, 2 columns and this plot is the 4 plot
-plt.subplot(2, 3, 5)
-plt.plot(x, y)
-# plot 6:
-x = np.array([0, 1, 2, 3])
-y = np.array([10, 20, 30, 40])
-# the figure has 1 row, 2 columns and this plot is the 4 plot
-plt.subplot(2, 3, 6)
-plt.plot(x, y)
-# Super title
-plt.suptitle("MY SHOP")
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# The scatter() function plots one dot for each observation
+plt.scatter(x, y)
+plt.show()
+# day one, the age and speed of 13 cars
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+colors = np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
+plt.scatter(x, y, color=colors)
+# day two, the age and speed of 15 cars
+x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+plt.scatter(x, y, color='#88c999')
+plt.show()
+#x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+#y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+#colors = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
+#sizes = np.array([20,50,100,200,500,1000,60,90,10,300,600,800,75])
+x = np.random.randint(100, size=(100))
+y = np.random.randint(100, size=(100))
+colors = np.random.randint(100, size=(100))
+sizes = 10 * np.random.randint(100, size=(100))
+plt.scatter(x, y, c = colors, cmap = 'nipy_spectral', s = sizes, alpha=0.5)
+plt.colorbar()
 plt.show()
