3e53c78aa00143d517c7e64b06018848c0c1af93
tudao <tudao@5nhatnhat.com>
2021-09-06 14:08:55 +0700

    - [Matplotlib]: Subplot (title, super title)

--- index.py
+++ index.py
@@ -2,14 +2,45 @@
 # The Pyplot package can be referred to as plt
 import matplotlib.pyplot as plt
 import numpy as np
-x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
-plt.title("Sports Watch Data")
-plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
+# subplot() function to draw multiple plots in one figure
+# plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+# the figure has 1 row, 2 columns and this plot is the first plot
+plt.subplot(2, 3, 1)
 plt.plot(x, y)
-# Add grid line to a plot using the grid() function
-# Display only grid lines for the x-axis
-# Set line properties for the grid
-plt.grid(axis='y', color='green', linestyle='--', linewidth=5.5)
+plt.title("SALES")
+# plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+# the figure has 1 row, 2 columns and this plot is the second plot
+plt.subplot(2, 3, 2)
+plt.plot(x, y)
+plt.title("INCOME")
+# plot 3:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+# the figure has 1 row, 2 columns and this plot is the 3 plot
+plt.subplot(2, 3, 3)
+plt.plot(x, y)
+# plot 4:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+# the figure has 1 row, 2 columns and this plot is the 4 plot
+plt.subplot(2, 3, 4)
+plt.plot(x, y)
+# plot 5:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+# the figure has 1 row, 2 columns and this plot is the 4 plot
+plt.subplot(2, 3, 5)
+plt.plot(x, y)
+# plot 6:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+# the figure has 1 row, 2 columns and this plot is the 4 plot
+plt.subplot(2, 3, 6)
+plt.plot(x, y)
+# Super title
+plt.suptitle("MY SHOP")
 plt.show()
