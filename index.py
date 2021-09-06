92e33d9029ba3f8f17b61f61992fcae9fa8d16a0
tudao <tudao@5nhatnhat.com>
2021-09-06 15:26:02 +0700

    - [Matplotlib]: Bar

--- index.py
+++ index.py
@@ -2,29 +2,11 @@
 # The Pyplot package can be referred to as plt
 import matplotlib.pyplot as plt
 import numpy as np
-x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
-y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
-# The scatter() function plots one dot for each observation
-plt.scatter(x, y)
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+plt.bar(x, y, color="red", width=0.1)
 plt.show()
-# day one, the age and speed of 13 cars
-x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
-y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
-colors = np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
-plt.scatter(x, y, color=colors)
-# day two, the age and speed of 15 cars
-x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
-y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
-plt.scatter(x, y, color='#88c999')
-plt.show()
-#x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
-#y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
-#colors = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
-#sizes = np.array([20,50,100,200,500,1000,60,90,10,300,600,800,75])
-x = np.random.randint(100, size=(100))
-y = np.random.randint(100, size=(100))
-colors = np.random.randint(100, size=(100))
-sizes = 10 * np.random.randint(100, size=(100))
-plt.scatter(x, y, c = colors, cmap = 'nipy_spectral', s = sizes, alpha=0.5)
-plt.colorbar()
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+plt.barh(x, y, color="#4CAF50", height=0.1)
 plt.show()
